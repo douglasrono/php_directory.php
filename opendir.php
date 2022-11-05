68b8a64fcbc9b4ddce68c1a96a70a4699bb76f69
Douglas Kiprono <69076570+douglasrono@users.noreply.github.com>
2022-11-05 05:49:20 +0300

    Create opendir.php

---
+++ opendir.php
@@ -0,0 +1,13 @@
+<?php
+$dir = "/images/";
+
+// Open a directory, and read its contents
+if (is_dir($dir)){
+  if ($dh = opendir($dir)){
+    while (($file = readdir($dh)) !== false){
+      echo "filename:" . $file . "<br>";
+    }
+    closedir($dh);
+  }
+}
+?>
